3f5f4918f87021fbaa5639da27e26bb051c4dece
PWRMind <aleksey.babanov@icloud.com>
2016-07-14 21:13:23 +0300

    Initial Commit

---
+++ DesignPatternsInSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DesignPatternsInSwift
+//
+//  Created by Алексей Бабанов on 14.07.16.
+//  Copyright © 2016 Алексей Бабанов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
